ec7ca8c14f55c25a11758cd670e383eb1573a043
yonghyeon0223 <80090053+yonghyeon0223@users.noreply.github.com>
2022-03-05 18:32:51 +0900

    Add files via upload

---
+++ wikiDocs/chapter3/CharString.java
@@ -0,0 +1,25 @@
+public class CharString{
+    public static void main(String[] args){
+
+    char a = 'a';
+    /*char b = "b"; error. Char needs to be in a pair of single quotation marks */
+    char b = 'b';
+
+    System.out.println(a+b); // 195. Does not produce 'ab' instead adds up ascii code number.
+
+    String c = "c";
+    String d = "d";
+
+    System.out.println(c+d); // "cd"
+// ---------------------------------------------------------------------------------
+
+    char a1 = 'a';
+    char a2 = 97;
+    char a3 = '\u0061';
+
+    System.out.println(String.format("%s, %s, %s", a1, a2, a3));
+
+
+
+    }
+}
