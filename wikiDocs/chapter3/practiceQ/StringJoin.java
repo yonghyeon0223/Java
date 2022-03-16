50b87ed9d943c64ad2c6d204e58a386d0ae8ccfe
yonghyeon0223 <80090053+yonghyeon0223@users.noreply.github.com>
2022-03-16 22:11:11 +1100

    Day 4 (git challenge :( )

---
+++ wikiDocs/chapter3/practiceQ/StringJoin.java
@@ -0,0 +1,14 @@
+package practiceQ;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.ArrayList;
+
+public class StringJoin {
+    public static void main(Stringp[] args){
+        String[] strArray = {"Hello", "My", "Friends", "I'm", "Isaac."};
+
+        ArrayList<String> strArrayList = new ArrayList<>(Arrays.asList(strArray));
+
+    }  
+}
